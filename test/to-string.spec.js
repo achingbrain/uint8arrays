0fa0604cc713b768a62838da0b2625c9a6625941
Cayman <caymannava@gmail.com>
2020-12-18 11:18:53 -0700

    feat: aegir types generation (#7)
    Minimal updates to add autogenerated typescript types via aegir.
    Used this doc to help: https://github.com/ipfs/aegir/blob/master/md/ts-jsdoc.md
    
    I tested this PR locally, linking this to a test project (including updated `web-encoding` dependency) to make sure the types all line up.
    
    See https://github.com/achingbrain/uint8arrays/pull/4#issuecomment-731109567
    Resolves #3

--- test/to-string.spec.js
+++ test/to-string.spec.js
@@ -8,7 +8,7 @@ const { TextEncoder } = require('web-encoding')
 describe('Uint8Array toString', () => {
   it('creates a String from a Uint8Array', () => {
     const str = 'hello world'
-    const arr = new TextEncoder('utf8').encode(str)
+    const arr = new TextEncoder().encode(str)
 
     expect(toString(arr)).to.deep.equal(str)
   })
