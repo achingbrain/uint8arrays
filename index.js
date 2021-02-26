42c7097c090d039f6b2c12ac7fb59208f9279718
Hugo Dias <hugomrdias@gmail.com>
2021-02-26 18:24:33 +0000

    fix: upgrade aegir and multibase

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 'use strict'
+const compare = require('./compare')
+const concat = require('./concat')
+const equals = require('./equals')
+const fromString = require('./from-string')
+const toString = require('./to-string')
+const xor = require('./xor')
 
 module.exports = {
-  compare: require('./compare'),
-  concat: require('./concat'),
-  equals: require('./equals'),
-  fromString: require('./from-string'),
-  toString: require('./to-string'),
-  xor: require('./xor')
+  compare,
+  concat,
+  equals,
+  fromString,
+  toString,
+  xor
 }
