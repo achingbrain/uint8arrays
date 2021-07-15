e0120546d6235dd78a62e0c10d0ba7bd68a9729c
achingbrain <alex@achingbrain.net>
2021-07-15 11:13:03 +0100

    chore: upgrade multiformats

--- util/bases.js
+++ util/bases.js
@@ -1,10 +1,6 @@
   'use strict'
 
-const b16 = require('multiformats/bases/base16')
-const b32 = require('multiformats/bases/base32')
-const b36 = require('multiformats/bases/base36')
-const b58 = require('multiformats/bases/base58')
-const b64 = require('multiformats/bases/base64')
+const { bases } = require('multiformats/basics')
 
 /**
  * @typedef {import('multiformats/bases/interface').MultibaseCodec<any>} MultibaseCodec
@@ -59,45 +55,21 @@ const ascii = createCodec('ascii', 'a', (buf) => {
 })
 
 /**
- * @typedef {'utf8' | 'utf-8' | 'hex' | 'latin1' | 'ascii' | 'binary'} NodeEncodings
- * @typedef {'base16' | 'base16upper'} Base16Encodings
- * @typedef {'base32' | 'base32upper' | 'base32pad' | 'base32padupper' | 'base32hex' | 'base32hexupper' | 'base32hexpad' | 'base32hexpadupper' | 'base32z'} Base32Encodings
- * @typedef {'base36' | 'base36upper'} Base36Encodings
- * @typedef {'base58btc' | 'base58flickr'} Base58Encodings
- * @typedef {'base64' | 'base64pad' | 'base64url' | 'base64urlpad'} Base64Encodings
- *
- * @typedef {NodeEncodings | Base16Encodings | Base32Encodings | Base36Encodings | Base58Encodings | Base64Encodings } SupportedEncodings
+ * @typedef {'utf8' | 'utf-8' | 'hex' | 'latin1' | 'ascii' | 'binary' | keyof bases } SupportedEncodings
  */
 
 /**
- * @type {Record<string, MultibaseCodec>}
+ * @type {Record<SupportedEncodings, MultibaseCodec>}
  */
 const BASES = {
   'utf8': string,
   'utf-8': string,
-  'hex': b16.base16,
+  'hex': bases.base16,
   'latin1': ascii,
   'ascii': ascii,
-  'binary': ascii
+  'binary': ascii,
+
+  ...bases
 }
-
-/**
- * @type {Record<string, any>}
- */
- const bases = {
-  ...b16,
-  ...b32,
-  ...b36,
-  ...b58,
-  ...b64
-}
-
-Object.keys(bases).forEach(key => {
-  const base = bases[key]
-
-  if (typeof base !== 'boolean') {
-    BASES[base.name] = base
-  }
-})
 
 module.exports = BASES
