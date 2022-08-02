2c00e61f2eb1f81a2a56b05d245edcab1e12d556
Alex Potsides <alex@achingbrain.net>
2022-08-02 13:43:15 +0100

    chore: add concat benchmark (#33)

---
+++ benchmarks/concat.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-console */
+
+/*
+$ node benchmarks/to-string.js
+$ npx playwright-test benchmarks/to-string.js --runner benchmark
+*/
+
+import Benchmark from 'benchmark'
+import { fromString } from '../src/from-string.js'
+import { concat } from '../src/concat.js'
+import { allocUnsafe } from '../src/alloc.js'
+
+const string = 'Hello world, this is a Uint8Array created from a string'
+const DATA1 = fromString(string)
+const DATA2 = fromString(string)
+
+function checkConcat (arr) {
+  if (arr.byteLength !== (DATA1.byteLength + DATA2.byteLength)) {
+    return false
+  }
+
+  for (let i = 0; i < arr.byteLength; i++) {
+    let offset = i
+    let bytes = DATA1
+
+    if (offset > bytes.byteLength) {
+      offset -= bytes.byteLength
+      bytes = DATA2
+    }
+
+    if (arr[i] !== bytes[offset]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+const suite = new Benchmark.Suite()
+
+suite
+  .add('Uint8Arrays.concat', () => {
+    const res = concat([DATA1, DATA2])
+
+    if (checkConcat(res)) {
+      throw new Error('Concat failed')
+    }
+  })
+  .add('Uint8Arrays.concat with length', () => {
+    const res = concat([DATA1, DATA2], DATA1.byteLength + DATA2.byteLength)
+
+    if (checkConcat(res)) {
+      throw new Error('Concat failed')
+    }
+  })
+  .add('Uint8Array.set', () => {
+    const res = new Uint8Array(DATA1.byteLength + DATA2.byteLength)
+    res.set(DATA1, 0)
+    res.set(DATA2, DATA1.byteLength)
+
+    if (checkConcat(res)) {
+      throw new Error('Concat failed')
+    }
+  })
+  .add('allocUnsafe.set', () => {
+    const res = allocUnsafe(DATA1.byteLength + DATA2.byteLength)
+    res.set(DATA1, 0)
+    res.set(DATA2, DATA1.byteLength)
+
+    if (checkConcat(res)) {
+      throw new Error('Concat failed')
+    }
+  })
+
+suite
+  // add listeners
+  .on('cycle', (event) => {
+    console.log(String(event.target))
+  })
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'))
+  })
+  // run async
+  .run({ async: true })
