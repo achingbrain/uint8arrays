bd7147f56bb40222dffc7e19688075b17527acf4
Alex Potsides <alex@achingbrain.net>
2022-08-02 10:49:44 +0100

    feat: add alloc and allocUnsafe (#31)
    Allows us to create `Uint8Array`s that reference unitialised memory where supported in order to improve performance.

---
+++ src/alloc.js
@@ -0,0 +1,30 @@
+/**
+ * Returns a `Uint8Array` of the requested size. Referenced memory will
+ * be initialized to 0.
+ *
+ * @param {number} [size]
+ * @returns {Uint8Array}
+ */
+export function alloc (size = 0) {
+  if (globalThis.Buffer != null && globalThis.Buffer.alloc != null) {
+    return globalThis.Buffer.alloc(size)
+  }
+
+  return new Uint8Array(size)
+}
+
+/**
+ * Where possible returns a Uint8Array of the requested size that references
+ * uninitialized memory. Only use if you are certain you will immediately
+ * overwrite every value in the returned `Uint8Array`.
+ *
+ * @param {number} [size]
+ * @returns {Uint8Array}
+ */
+export function allocUnsafe (size = 0) {
+  if (globalThis.Buffer != null && globalThis.Buffer.allocUnsafe != null) {
+    return globalThis.Buffer.allocUnsafe(size)
+  }
+
+  return new Uint8Array(size)
+}
