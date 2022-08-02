1cf09325be80e1026d862a5c834c4e1ad4098892
achingbrain <alex@achingbrain.net>
2022-08-02 12:17:44 +0100

    chore: benchmark more methods

--- benchmarks/to-string.js
+++ benchmarks/to-string.js
@@ -7,12 +7,13 @@ $ npx playwright-test benchmarks/to-string.js --runner benchmark
 
 import Benchmark from 'benchmark'
 import { fromString } from '../src/from-string.js'
+import { toString } from '../src/to-string.js'
 
 const string = 'Hello world, this is a Uint8Array created from a string'
 const DATA = fromString(string)
 
 // https://github.com/achingbrain/uint8arrays/issues/30#issuecomment-1199120924
-function utf8Read (buffer, start, end) {
+function utf8ReadFromCharCode (buffer, start, end) {
   const len = end - start
   if (len < 1) { return '' }
   let parts = null
@@ -38,9 +39,41 @@ function utf8Read (buffer, start, end) {
   return String.fromCharCode.apply(String, chunk.slice(0, i))
 }
 
+// https://github.com/achingbrain/uint8arrays/issues/30#issuecomment-1199120924
+function utf8ReadConcat(buffer, start, end) {
+  if (end - start < 1) {
+      return "";
+  }
+
+  var str = "";
+  for (var i = start; i < end;) {
+      var t = buffer[i++];
+      if (t <= 0x7F) {
+          str += String.fromCharCode(t);
+      } else if (t >= 0xC0 && t < 0xE0) {
+          str += String.fromCharCode((t & 0x1F) << 6 | buffer[i++] & 0x3F);
+      } else if (t >= 0xE0 && t < 0xF0) {
+          str += String.fromCharCode((t & 0xF) << 12 | (buffer[i++] & 0x3F) << 6 | buffer[i++] & 0x3F);
+      } else if (t >= 0xF0) {
+          var t2 = ((t & 7) << 18 | (buffer[i++] & 0x3F) << 12 | (buffer[i++] & 0x3F) << 6 | buffer[i++] & 0x3F) - 0x10000;
+          str += String.fromCharCode(0xD800 + (t2 >> 10));
+          str += String.fromCharCode(0xDC00 + (t2 & 0x3FF));
+      }
+  }
+
+  return str;
+};
+
 const suite = new Benchmark.Suite()
 
 suite
+  .add('Uint8Arrays.toString', () => {
+    const res = toString(DATA)
+
+    if (res !== string) {
+      throw new Error('String encoding failed')
+    }
+  })
   .add('TextDecoder', () => {
     const res = new TextDecoder().decode(DATA)
 
@@ -48,8 +81,15 @@ suite
       throw new Error('String encoding failed')
     }
   })
-  .add('utf8Read', () => {
-    const res = utf8Read(DATA, 0, DATA.byteLength)
+  .add('utf8ReadFromCharCode', () => {
+    const res = utf8ReadFromCharCode(DATA, 0, DATA.byteLength)
+
+    if (res !== string) {
+      throw new Error('String encoding failed')
+    }
+  })
+  .add('utf8ReadConcat', () => {
+    const res = utf8ReadConcat(DATA, 0, DATA.byteLength)
 
     if (res !== string) {
       throw new Error('String encoding failed')
